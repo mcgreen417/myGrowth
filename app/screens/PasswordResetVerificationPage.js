0d41978c3f33b2547bd464671692de14412f5341
emsonn <eabrahamson@knights.ucf.edu>
2021-02-17 04:18:12 -0500

    Updated PasswordResetVerificationPage with new global styles

--- app/screens/PasswordResetVerificationPage.js
+++ app/screens/PasswordResetVerificationPage.js
@@ -14,13 +14,13 @@ import {
 function PasswordResetVerificationPage({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#A5DFB2' barStyle='light-content' />
+      <StatusBar backgroundColor={global.statusBarColor} barStyle='light-content' />
       <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
         {/* Logo + title + page instructions */}
         <Image style={styles.logo} source={require('../assets/icon.png')} />
-        <Text style={{ fontWeight: 'bold', color: '#816868', fontSize: 44 }}>myGrowth</Text>
-        <Text style={{ fontWeight: 'bold', color: '#816868', fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
-        <Text style={{ color: '#816868', marginBottom: 12, textAlign: 'center' }}>
+        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 44 }}>myGrowth</Text>
+        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
+        <Text style={{ color: global.textColor, marginBottom: 12, textAlign: 'center' }}>
           A password reset code has been sent to your e-mail.{"\n"}
           Enter the password reset code you received below.
         </Text>
@@ -28,22 +28,22 @@ function PasswordResetVerificationPage({ navigation }) {
         <View style={styles.buttons}>
           <TextInput style={styles.textInput} placeholder='Reset Code' />
           <View style={{ marginVertical: 8 }} />
-          <Button title='VERIFY' color='#A5DFB2' onPress={() => navigation.navigate('ResetPasswordPage')} />
+          <Button title='VERIFY' color={global.optionButtonsColor} onPress={() => navigation.navigate('ResetPasswordPage')} />
           <View style={{ marginVertical: 8 }} />
         </View>
         {/* Resend password reset code */}
         <View>
           <View style={{ flexDirection: 'row' }}>
-            <Text style={{ color: '#816868' }}>Didn't receive a reset code? </Text>
+            <Text style={{ color: global.textColor }}>Didn't receive a reset code? </Text>
             <TouchableOpacity>
-              <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Resend e-mail.</Text>
+              <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Resend e-mail.</Text>
             </TouchableOpacity>
           </View>
         </View>
         {/* Login page redirect */}
         <View style={{ flexDirection: 'row', marginTop: 8 }}>
           <TouchableOpacity onPress={() => navigation.navigate('LoginPage')}>
-            <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Return to login.</Text>
+            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Return to login.</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -54,7 +54,7 @@ function PasswordResetVerificationPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F6EFED',
+    backgroundColor: global.pageBackgroundColor,
   },
   logo: {
     width: 100,
@@ -64,15 +64,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    borderColor: 'black',
+    borderColor: global.optionButtonsBorderColor,
   },
   textInput: {
     height: 40,
-    borderColor: '#A5DFB2',
+    borderColor: global.textInputBorderColor,
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-    color: '#000000',
+    backgroundColor: global.textInputFillColor,
+    color: global.textInputColor,
     textAlign: 'center',
   },
 });
