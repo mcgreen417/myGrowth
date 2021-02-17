579fe9ee492ff6c1903520ef1622cc2d84f12ee5
emsonn <eabrahamson@knights.ucf.edu>
2021-02-17 03:49:40 -0500

    Updated LoginPage with new global styles

--- app/screens/LoginPage.js
+++ app/screens/LoginPage.js
@@ -15,44 +15,44 @@ import {
 function LoginPage({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#A5DFB2' barStyle='light-content' />
+      <StatusBar backgroundColor={global.statusBarColor} barStyle='light-content' />
       <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
         {/* Logo + title */}
         <Image style={styles.logo} source={require('../assets/icon.png')} />
-        <Text style={{fontWeight: 'bold', color: '#816868', fontSize: 36}}>myGrowth</Text>
-        <Text style={{fontWeight: 'bold', color: '#816868', fontSize: 16, marginBottom: 20}}>Your General Wellness Tracker</Text>
+        <Text style={{fontWeight: 'bold', color: global.textColor, fontSize: 36}}>myGrowth</Text>
+        <Text style={{fontWeight: 'bold', color: global.textColor, fontSize: 16, marginBottom: 20}}>Your General Wellness Tracker</Text>
         {/* E-mail address + password entry boxes, login button */}
         <View style={styles.buttons}>
           <TextInput style={styles.textInput} placeholder='E-mail Address' />
           <View style={{ marginVertical: 8 }} />
           <TextInput style={styles.textInput} placeholder='Password' secureTextEntry={true} />
           <View style={{ marginVertical: 8 }} />
-          <Button title='LOG IN' color='#A5DFB2' onPress={ () => signIn() } />
+          <Button title='LOG IN' color={global.optionButtonsColor} onPress={ () => signIn() } />
           <View style={{ marginVertical: 8 }} />
         </View>
         {/* Login/signup page switch */}
         <View style={{ flexDirection: 'row' }}>
-          <Text style={{ color: '#816868' }}>Don't have an account? </Text>
+          <Text style={{ color: global.textColor }}>Don't have an account? </Text>
           <TouchableOpacity onPress={() => navigation.navigate('SignUpPage')}>
-            <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Sign up here.</Text>
+            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Sign up here.</Text>
           </TouchableOpacity>
         </View>
         <View>
           <TouchableOpacity onPress={() => navigation.navigate('ForgotPasswordPage')}>
-            <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Forgot your password?</Text>
+            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Forgot your password?</Text>
           </TouchableOpacity>
         </View>
         {/* TOS + privacy policy agreement */}
         <View style={{ marginVertical: 8 }} />
         <View>
-          <Text style={{ color: '#816868' }}>By continuing, you're accepting our </Text>
+          <Text style={{ color: global.textColor }}>By continuing, you're accepting our </Text>
           <View style={{ flexDirection: 'row' }}>
             <TouchableOpacity>
-              <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Terms of Service</Text>
+              <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Terms of Service</Text>
             </TouchableOpacity>
-            <Text style={{ color: '#816868' }}> and </Text>
+            <Text style={{ color: global.textColor }}> and </Text>
             <TouchableOpacity>
-              <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Privacy Policy.</Text>
+              <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Privacy Policy.</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -99,7 +99,7 @@ async function testQuery(username) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F6EFED',
+    backgroundColor: global.pageBackgroundColor,
   },
   logo: {
     width: 100,
@@ -109,15 +109,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    borderColor: 'black',
+    borderColor: global.optionButtonsBorderColor,
   },
   textInput: {
     height: 40,
-    borderColor: '#A5DFB2',
+    borderColor: global.textInputBorderColor,
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-    color: '#000000',
+    backgroundColor: global.textInputFillColor,
+    color: global.textInputColor,
     textAlign: 'center',
   },
 });
