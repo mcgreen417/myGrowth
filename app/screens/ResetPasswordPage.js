1928b63f3e4728c018526ae9c73761e71f82afd1
emsonn <eabrahamson@knights.ucf.edu>
2021-02-17 04:26:42 -0500

    Updated ResetPasswordPage with new global styles

--- app/screens/ResetPasswordPage.js
+++ app/screens/ResetPasswordPage.js
@@ -14,13 +14,13 @@ import {
 function ResetPasswordPage({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor='#A5DFB2' barStyle='light-content' />
+      <StatusBar backgroundColor={global.statusBarColor} barStyle='light-content' />
       <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
         {/* Logo + title + page instructions */}
         <Image style={styles.logo} source={require('../assets/icon.png')} />
-        <Text style={{ fontWeight: 'bold', color: '#816868', fontSize: 44 }}>myGrowth</Text>
-        <Text style={{ fontWeight: 'bold', color: '#816868', fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
-        <Text style={{ color: '#816868', marginBottom: 12, textAlign: 'center' }}>
+        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 44 }}>myGrowth</Text>
+        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
+        <Text style={{ color: global.textColor, marginBottom: 12, textAlign: 'center' }}>
           You may now reset your password.{"\n"}
           Enter your new password below.</Text>
         {/* New password + confirm password buttons, submit button */}
@@ -29,13 +29,13 @@ function ResetPasswordPage({ navigation }) {
           <View style={{ marginVertical: 8 }} />
           <TextInput style={styles.textInput} placeholder='Confirm New Password' secureTextEntry={true} />
           <View style={{ marginVertical: 8 }} />
-          <Button title='SUBMIT' color='#A5DFB2' onPress={() => navigation.navigate('LoginPage')} />
+          <Button title='SUBMIT' color={global.optionButtonsColor} onPress={() => navigation.navigate('LoginPage')} />
           <View style={{ marginVertical: 8 }} />
         </View>
         {/* Login page redirect */}
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => navigation.navigate('LoginPage')}>
-            <Text style={{ color: '#A5DFB2', textDecorationLine: 'underline' }}>Return to login.</Text>
+            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Return to login.</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -46,7 +46,7 @@ function ResetPasswordPage({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F6EFED',
+    backgroundColor: global.pageBackgroundColor,
   },
   logo: {
     width: 100,
@@ -56,15 +56,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    borderColor: 'black',
+    borderColor: global.optionButtonsBorderColor,
   },
   textInput: {
     height: 40,
-    borderColor: '#A5DFB2',
+    borderColor: global.textInputBorderColor,
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-    color: '#000000',
+    backgroundColor: global.textInputFillColor,
+    color: global.textInputColor,
     textAlign: 'center',
   },
 });
