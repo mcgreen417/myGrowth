b1cb68086ca9eeb01d1973746f0ed4525fb1aa2d
Enelson Castro <enecastro@knights.ucf.edu>
2021-02-17 22:16:26 -0500

    Added base menu, will make menu buttons that link to other pages

--- app/home/screens/Home.js
+++ app/home/screens/Home.js
@@ -7,16 +7,155 @@ import {
   Image,
   Button,
 } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+const NavBar = () => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        position: 'absolute',
+        zIndex: 1,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 60,
+        width: '100%',
+      }}
+    >
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='home' color='#F5F5F5' />
+        <Text>Home</Text>
+      </View>
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='bookmark' color='#F5F5F5' />
+        <Text>Journal</Text>
+      </View>
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='star' color='#F5F5F5' />
+        <Text>Goals</Text>
+      </View>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+          zIndex: 3,
+        }}
+      >
+        <View
+          style={{
+            width: 70,
+            height: 70,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#F6EFED',
+            borderRadius: 70 / 2,
+            margin: -20,
+            bottom: 40,
+          }}
+        >
+          <View
+            style={{
+              width: 56,
+              height: 56,
+              justifyContent: 'center',
+              backgroundColor: '#4CB97A',
+              borderRadius: 56 / 2,
+            }}
+          >
+            <Icon name='plus' type='font-awesome' color='#F5F5F5' />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='check' color='#F5F5F5' />
+        <Text>To-Do</Text>
+      </View>
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='history' color='#F5F5F5' />
+        <Text>History</Text>
+      </View>
+      <View
+        style={{
+          padding: 5,
+          flex: 1,
+          zIndex: 2,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#A5DFB2',
+        }}
+      >
+        <Icon name='menu' color='#F5F5F5' />
+        <Text numberOfLines={1} style={{ color: '#F5F5F5' }}>
+          Account
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 const HomePage = ({ navigation }) => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View>
         <Text>Good Morning, Firstname!</Text>
-        <Image source={require('../../shared/assets/icon.png')} />
+        <Image
+          source={require('../../shared/assets/icon.png')}
+          style={styles.avatar}
+        />
       </View>
       <View>
-        <Image source={require('../../shared/assets/icon.png')} />
+        <Image
+          source={require('../../shared/assets/icon.png')}
+          style={styles.avatar}
+        />
       </View>
       <View>
         <Button title='<- Customize plant' />
@@ -27,7 +166,10 @@ const HomePage = ({ navigation }) => {
         <Button title='View past entries ->' />
       </View>
       <View>
-        <Image source={require('../../shared/assets/icon.png')} />
+        <Image
+          source={require('../../shared/assets/icon.png')}
+          style={styles.avatar}
+        />
         <View>
           <Text>Why don't you try doing one of these?</Text>
           <Button title='Write a journal entry' />
@@ -36,10 +178,27 @@ const HomePage = ({ navigation }) => {
           <Button title='View your history' />
         </View>
       </View>
+      <NavBar />
     </SafeAreaView>
   );
 };
 
 export default HomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6EFED',
+  },
+  avatar: {
+    width: 75,
+    height: 75,
+    marginRight: 24,
+  },
+  buttons: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: 100,
+    height: 50,
+  },
+});
