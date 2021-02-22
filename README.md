52543c9c5dbd999247dce20c9f2f7475f9264f2a
Enelson Castro <enecastro@knights.ucf.edu>
2021-02-22 01:31:33 -0500

    Some more changes to merge with main.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # myGrowth
 
 ### Your General Wellness Tracker
+
+Requires `.env` file to connect to AWS resources
+
+```
+COGNITO_REGION=
+USER_POOL_ID=
+USER_POOL_WEB_CLIENT_ID=
+AWS_APPSYNC_GRAPHQL_ENDPOINT=
+AWS_APPSYNC_REGION=
+AWS_APPSYNC_AUTHENTICATION_TYPE=
+```
--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,6 @@ module.exports = function (api) {
   api.cache(false);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', { moduleName: '@env', path: '.env' }],
-    ],
+    plugins: ['module:react-native-dotenv'],
   };
 };
