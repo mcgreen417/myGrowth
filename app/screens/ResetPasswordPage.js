79b0a905f12d776c14160ed17d8bc6fb026b174f
emsonn <eabrahamson@knights.ucf.edu>
2021-02-25 15:33:12 -0500

    Updated ResetPasswordPage with colorblind mode funtionality

--- app/screens/ResetPasswordPage.js
+++ app/screens/ResetPasswordPage.js
@@ -13,29 +13,29 @@ import {
 
 function ResetPasswordPage({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor={global.statusBarColor} barStyle='light-content' />
+    <SafeAreaView style={styles().container}>
+      <StatusBar backgroundColor={(global.colorblindMode ? global.cb_statusBarColor : global.statusBarColor)} barStyle='light-content' />
       <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
         {/* Logo + title + page instructions */}
-        <Image style={styles.logo} source={require('../assets/icon.png')} />
-        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 44 }}>myGrowth</Text>
-        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
-        <Text style={{ color: global.textColor, marginBottom: 12, textAlign: 'center' }}>
+        <Image style={styles().logo} source={require('../assets/icon.png')} />
+        <Text style={{ fontWeight: 'bold', color: (global.colorblindMode ? global.cb_textColor : global.textColor), fontSize: 44 }}>myGrowth</Text>
+        <Text style={{ fontWeight: 'bold', color: (global.colorblindMode ? global.cb_textColor : global.textColor), fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
+        <Text style={{ color: (global.colorblindMode ? global.cb_textColor : global.textColor), marginBottom: 12, textAlign: 'center' }}>
           You may now reset your password.{"\n"}
           Enter your new password below.</Text>
         {/* New password + confirm password buttons, submit button */}
-        <View style={styles.buttons}>
-          <TextInput style={styles.textInput} placeholder='New Password' secureTextEntry={true} />
+        <View style={styles().buttons}>
+          <TextInput style={styles().textInput} placeholder='New Password' secureTextEntry={true} placeholderTextColor={(global.colorblindMode ? global.cb_placeHolderTextColor : global.placeholderTextColor)} />
           <View style={{ marginVertical: 8 }} />
-          <TextInput style={styles.textInput} placeholder='Confirm New Password' secureTextEntry={true} />
+          <TextInput style={styles().textInput} placeholder='Confirm New Password' secureTextEntry={true} placeholderTextColor={(global.colorblindMode ? global.cb_placeHolderTextColor : global.placeholderTextColor)} />
           <View style={{ marginVertical: 8 }} />
-          <Button title='SUBMIT' color={global.optionButtonsColor} onPress={() => navigation.navigate('LoginPage')} />
+          <Button title='SUBMIT' color={(global.colorblindMode ? global.cb_optionButtonsColor : global.optionButtonsColor)} onPress={() => navigation.navigate('LoginPage')} />
           <View style={{ marginVertical: 8 }} />
         </View>
         {/* Login page redirect */}
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={() => navigation.navigate('LoginPage')}>
-            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Return to login.</Text>
+            <Text style={{ color: (global.colorblindMode ? global.cb_hyperlinkedTextColor : global.hyperlinkedTextColor), textDecorationLine: 'underline' }}>Return to login.</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -43,10 +43,10 @@ function ResetPasswordPage({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = () => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: global.pageBackgroundColor,
+    backgroundColor: (global.colorblindMode ? global.cb_pageBackgroundColor : global.pageBackgroundColor),
   },
   logo: {
     width: 100,
@@ -56,15 +56,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    borderColor: global.optionButtonsBorderColor,
+    borderColor: (global.colorblindMode ? global.cb_optionButtonsColor : global.optionButtonsBorderColor),
   },
   textInput: {
     height: 40,
-    borderColor: global.textInputBorderColor,
+    borderColor: (global.colorblindMode ? global.cb_textInputBorderColor : global.textInputBorderColor),
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: global.textInputFillColor,
-    color: global.textInputColor,
+    backgroundColor: (global.colorblindMode ? global.cb_textInputFillColor : global.textInputFillColor),
+    color: (global.colorblindMode ? global.cb_textInputcolor : global.textInputColor),
     textAlign: 'center',
   },
 });
