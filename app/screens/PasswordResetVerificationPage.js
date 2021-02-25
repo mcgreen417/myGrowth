34776b6bd2f0f3afd63adaafbabc8554ad7a9afd
emsonn <eabrahamson@knights.ucf.edu>
2021-02-25 14:24:21 -0500

    PasswordResetVerificationPage updated with colorblind mode functionality

--- app/screens/PasswordResetVerificationPage.js
+++ app/screens/PasswordResetVerificationPage.js
@@ -13,37 +13,37 @@ import {
 
 function PasswordResetVerificationPage({ navigation }) {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor={global.statusBarColor} barStyle='light-content' />
+    <SafeAreaView style={styles().container}>
+      <StatusBar backgroundColor={(global.colorblindMode ? global.cb_statusBarColor : global.statusBarColor)} barStyle='light-content' />
       <View style={{ justifyContent: 'center', alignItems: 'center', height: '100%' }}>
         {/* Logo + title + page instructions */}
-        <Image style={styles.logo} source={require('../assets/icon.png')} />
-        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 44 }}>myGrowth</Text>
-        <Text style={{ fontWeight: 'bold', color: global.textColor, fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
-        <Text style={{ color: global.textColor, marginBottom: 12, textAlign: 'center' }}>
+        <Image style={styles().logo} source={require('../assets/icon.png')} />
+        <Text style={{ fontWeight: 'bold', color: (global.colorblindMode ? global.cb_textColor : global.textColor), fontSize: 44 }}>myGrowth</Text>
+        <Text style={{ fontWeight: 'bold', color: (global.colorblindMode ? global.cb_textColor : global.textColor), fontSize: 20, marginBottom: 16 }}>Your General Wellness Tracker</Text>
+        <Text style={{ color: (global.colorblindMode ? global.cb_textColor : global.textColor), marginBottom: 12, textAlign: 'center' }}>
           A password reset code has been sent to your e-mail.{"\n"}
           Enter the password reset code you received below.
         </Text>
         {/* Reset code text entry + verify button */}
-        <View style={styles.buttons}>
-          <TextInput style={styles.textInput} placeholder='Reset Code' />
+        <View style={styles().buttons}>
+          <TextInput style={styles().textInput} placeholder='Reset Code' />
           <View style={{ marginVertical: 8 }} />
-          <Button title='VERIFY' color={global.optionButtonsColor} onPress={() => navigation.navigate('ResetPasswordPage')} />
+          <Button title='VERIFY' color={(global.colorblindMode ? global.cb_optionBUttonsColor : global.optionButtonsColor)} onPress={() => navigation.navigate('ResetPasswordPage')} />
           <View style={{ marginVertical: 8 }} />
         </View>
         {/* Resend password reset code */}
         <View>
           <View style={{ flexDirection: 'row' }}>
-            <Text style={{ color: global.textColor }}>Didn't receive a reset code? </Text>
+            <Text style={{ color: (global.colorblindMode ? global.cb_textColor : global.textColor) }}>Didn't receive a reset code? </Text>
             <TouchableOpacity>
-              <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Resend e-mail.</Text>
+              <Text style={{ color: (global.colorblindMode ? global.cb_hyperlinkedTextColor : global.hyperLinkedTextColor), textDecorationLine: 'underline' }}>Resend e-mail.</Text>
             </TouchableOpacity>
           </View>
         </View>
         {/* Login page redirect */}
         <View style={{ flexDirection: 'row', marginTop: 8 }}>
           <TouchableOpacity onPress={() => navigation.navigate('LoginPage')}>
-            <Text style={{ color: global.hyperlinkedTextColor, textDecorationLine: 'underline' }}>Return to login.</Text>
+            <Text style={{ color: (global.colorblindMode ? global.cb_hyperlinkedTextColor : global.hyperlinkedTextColor), textDecorationLine: 'underline' }}>Return to login.</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -51,10 +51,10 @@ function PasswordResetVerificationPage({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = () => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: global.pageBackgroundColor,
+    backgroundColor: (global.colorblindMode ? global.cb_pageBackgroundColor : global.pageBackgroundColor),
   },
   logo: {
     width: 100,
@@ -64,15 +64,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     width: 300,
-    borderColor: global.optionButtonsBorderColor,
+    borderColor: (global.colorblindMode ? global.cb_optionButtonsBorderColor : global.optionButtonsBorderColor),
   },
   textInput: {
     height: 40,
-    borderColor: global.textInputBorderColor,
+    borderColor: (global.colorblindMode ? global.cb_textInputBorderColor : global.textInputBorderColor),
     borderWidth: 2,
     borderRadius: 10,
-    backgroundColor: global.textInputFillColor,
-    color: global.textInputColor,
+    backgroundColor: (global.colorblindMode ? global.cb_textInputFillColor : global.textInputFillColor),
+    color: (global.colorblindMode ? global.cb_textInputColor : global.textInputColor),
     textAlign: 'center',
   },
 });
