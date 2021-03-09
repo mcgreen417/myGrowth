bb7ef76434c29e2d7f8f8d878aebadbd42c96a56
adenrae <99aden@gmail.com>
2021-03-09 16:11:37 -0500

    preliminary outline for component
    brainstorming structure of component

--- app/shared/components/TabBarAndContent.js
+++ app/shared/components/TabBarAndContent.js
@@ -1,11 +1,121 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Image,
+    Button,
+    Pressable,
+  } from 'react-native';
+  import { Icon } from 'react-native-elements';
 
-export class TabBarAndContent extends Component {
-    render() {
-        return(
-            <div className="tabBarComp">
-                
-            </div>
-        );
-    }
-}
+  const TabBarandContent = ({
+      history = false,
+      historyGenComp = false,
+      dailyActivities = false,
+      generalHealth = false,
+      medication = false,
+      sleep = false,
+      fitness = false,
+  }) => {
+    return (
+        <View>
+            {/* History Comp version */}
+            <View>
+                {/* History */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* Gen comp version: mood, stress, period, meal */}
+            <View>
+                {/* History */}
+                <Pressable>
+
+                </Pressable>
+                {/* Correlation */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* daily activities */}
+            <View>
+                {/* history */}
+                <Pressable>
+
+                </Pressable>
+                {/* activity view */}
+                <Pressable>
+
+                </Pressable>
+                {/* correlation */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* general health */}
+            <View>
+                {/* history */}
+                <Pressable>
+
+                </Pressable>
+                {/* intensity */}
+                <Pressable>
+
+                </Pressable>
+                {/* correlation */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* medication */}
+            <View>
+                {/* prescription */}
+                <Pressable>
+
+                </Pressable>
+                {/* correlation */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* sleep */}
+            <View>
+                {/* time asleep */}
+                <Pressable>
+
+                </Pressable>
+                {/* quality */}
+                <Pressable>
+
+                </Pressable>
+                {/* correlation */}
+                <Pressable>
+
+                </Pressable>
+            </View>
+
+            {/* fitness */}
+            <View>
+                {/* history */}
+                <Pressable>
+
+                </Pressable>
+                {/* exercises */}
+                <Pressable>
+
+                </Pressable>
+                {/* correlation */}
+                <Pressable>
+                    
+                </Pressable>
+            </View>
+        </View>
+    );
+  };
