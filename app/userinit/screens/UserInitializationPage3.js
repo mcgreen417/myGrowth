ae593d5e1471ae15ca07ed223e6f2225e70b4f80
emsonn <eabrahamson@knights.ucf.edu>
2021-03-13 13:26:36 -0500

    Updated with colorblind mode functionality

--- app/userinit/screens/UserInitializationPage3.js
+++ app/userinit/screens/UserInitializationPage3.js
@@ -48,168 +48,296 @@ function UserInitializationPage3({ navigation }) {
     setUseFitnessTracking((previousState) => !previousState);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.pageSetup}>
+    <SafeAreaView style={styles().container}>
+      <View style={styles().pageSetup}>
         {/* Gardener avatar + page blurb */}
-        <View style={styles.avatarView}>
+        <View style={styles().avatarView}>
           <Image
-            style={styles.avatar}
+            style={styles().avatar}
             source={require('../../shared/assets/gardener-avatar.png')}
           />
-          <Text style={styles.pageDescription}>
+          <Text style={styles().pageDescription}>
             Edit your user settings below. These user settings may be changed at
             any time by returning to this page.
           </Text>
         </View>
         {/* Top page divider */}
-        <View style={styles.dividerView}>
-          <View style={styles.divider} />
+        <View style={styles().dividerView}>
+          <View style={styles().divider} />
         </View>
 
         {/* Health entry settings */}
-        <Text style={styles.heading}>HEALTH ENTRY SETTINGS</Text>
-        <View style={styles.line} />
+        <Text style={styles().heading}>HEALTH ENTRY SETTINGS</Text>
+        <View style={styles().line} />
 
         {/* Stress levels */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Stress Levels</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Stress Levels</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useStressLevels ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useStressLevels
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleStressLevels}
               value={useStressLevels}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Daily activities */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Daily Activities</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Daily Activities</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useDailyActivities ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useDailyActivities
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleDailyActivities}
               value={useDailyActivities}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Weight tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Weight Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Weight Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useWeightTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useWeightTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleWeightTracking}
               value={useWeightTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Period tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Period Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Period Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={usePeriodTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                usePeriodTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={togglePeriodTracking}
               value={usePeriodTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Medication tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Medication Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Medication Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useMedicationTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useMedicationTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleMedicationTracking}
               value={useMedicationTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Sleep tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Sleep Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Sleep Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useSleepTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useSleepTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleSleepTracking}
               value={useSleepTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Meal tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Meal Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Meal Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useMealTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useMealTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleMealTracking}
               value={useMealTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Fitness tracking */}
-        <View style={styles.inlineRow}>
-          <Text style={styles.textReg}>Fitness Tracking</Text>
-          <View style={styles.switchView}>
-            <View style={styles.line2} />
+        <View style={styles().inlineRow}>
+          <Text style={styles().textReg}>Fitness Tracking</Text>
+          <View style={styles().switchView}>
+            <View style={styles().line2} />
             <Switch
-              trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
-              thumbColor={useFitnessTracking ? '#4CB97A' : '#f4f3f4'}
-              ios_backgroundColor='#3e3e3e'
+              trackColor={{ 
+                false: global.colorblindMode 
+                  ? global.cb_switchTrackColorFalse
+                  : global.switchTrackColorFalse,
+                true: global.colorblindMode
+                  ? global.cb_switchTrackColorTrue 
+                  : global.switchTrackColorTrue
+              }}
+              thumbColor={
+                useFitnessTracking
+                  ? (global.colorblindMode 
+                      ? global.cb_switchThumbColorTrue
+                      : global.switchThumbColorTrue)
+                  : (global.colorblindMode
+                      ? global.cb_switchThumbColorFalse
+                      : global.switchThumbColorFalse)
+              }
+              ios_backgroundColor={global.cb_blackColor}
               onValueChange={toggleFitnessTracking}
               value={useFitnessTracking}
             />
           </View>
         </View>
-        <View style={styles.line} />
+        <View style={styles().line} />
 
         {/* Back & next buttons */}
         <View style={{ marginTop: '33%' }} />
-        <View style={styles.buttonsContainer}>
+        <View style={styles().buttonsContainer}>
           <Button
             title='Back'
-            color='#A5DFB2'
+            color={
+              global.colorblindMode
+                ? global.cb_optionButtonsColor
+                : global.optionButtonsColor
+            }
             onPress={() => navigation.navigate('UserInitializationPage2')}
           />
           <View style={{ width: '70%' }}></View>
           <Button
             title='Finish'
-            color='#A5DFB2'
+            color={
+              global.colorblindMode
+                ? global.cb_optionButtonsColor
+                : global.optionButtonsColor
+            }
             onPress={() => navigation.navigate('HomePage')}
           />
         </View>
@@ -220,10 +348,12 @@ function UserInitializationPage3({ navigation }) {
 
 export default UserInitializationPage3;
 
-const styles = StyleSheet.create({
+const styles = () => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F6EFED',
+    backgroundColor: global.colorblindMode
+      ? global.cb_pageBackgroundColor
+      : global.pageBackgroundColor,
   },
   avatar: {
     width: 75,
@@ -260,7 +390,9 @@ const styles = StyleSheet.create({
   divider: {
     flex: 1,
     height: 1,
-    backgroundColor: '#816868',
+    backgroundColor: global.colorblindMode
+      ? global.cb_contentDividerColor
+      : global.contentDividerColor,
     marginLeft: 20,
     marginRight: 20,
   },
@@ -271,7 +403,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   heading: {
-    color: '#816868',
+    color: global.colorblindMode
+      ? global.cb_textColor
+      : global.textColor,
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'left',
@@ -285,12 +419,16 @@ const styles = StyleSheet.create({
   },
   line: {
     width: '90%',
-    borderColor: '#816868',
+    borderColor: global.colorblindMode
+      ? global.cb_lineColor
+      : global.lineColor,
     borderBottomWidth: 1,
     minHeight: 1,
   },
   line2: {
-    borderColor: '#816868',
+    borderColor: global.colorblindMode
+      ? global.cb_lineColor
+      : global.lineColor,
     borderRightWidth: 1,
     minHeight: 28,
     marginTop: 4,
@@ -302,11 +440,15 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   textReg: {
-    color: 'black',
+    color: global.colorblindMode
+      ? global.cb_textColor
+      : global.textColor,
     textAlign: 'left',
   },
   pageDescription: {
-    color: '#816868',
+    color: global.colorblindMode
+      ? global.cb_textColor
+      : global.textColor,
     fontSize: 16,
     flex: 1,
     flexWrap: 'wrap',
@@ -329,15 +471,23 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     width: '70%',
-    backgroundColor: '#f4f3f4',
+    backgroundColor: global.colorblindMode
+      ? global.cb_textInputFillColor
+      : global.textInputFillColor,
   },
   textInput: {
     height: 40,
-    borderColor: 'black',
+    borderColor: global.colorblindMode
+      ? global.cb_textInputBlackBorderColor
+      : global.textInputBlackBorderColor,
     borderWidth: 1,
     borderRadius: 4,
-    backgroundColor: '#f4f3f4',
-    color: '#000000',
+    backgroundColor: global.colorblindMode
+      ? global.cb_textInputFillColor
+      : global.textInputFillColor,
+    color: global.colorblindMode
+      ? global.cb_textInputColor
+      : global.textInputColor,
     width: '70%',
     paddingLeft: 10,
     fontSize: 16,
@@ -350,11 +500,17 @@ const styles = StyleSheet.create({
   },
   textInput2: {
     height: 36,
-    borderColor: 'black',
+    borderColor: global.colorblindMode
+      ? global.cb_textInputBlackBorderColor
+      : global.textInputBlackBorderColor,
     borderWidth: 1,
     borderRadius: 4,
-    backgroundColor: '#f4f3f4',
-    color: '#000000',
+    backgroundColor: global.colorblindMode
+      ? global.cb_textInputFillColor
+      : global.textInputFillColor,
+    color: global.colorblindMode
+      ? global.cb_textInputColor
+      : global.textInputColor,
     width: '12%',
     textAlign: 'center',
     fontSize: 16,
