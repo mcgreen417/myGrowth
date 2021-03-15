19319a97f8e156335a396ebe3e5fbbddfdfaddd4
emsonn <eabrahamson@knights.ucf.edu>
2021-03-15 13:59:00 -0400

    Unfinished page, updated ToDoList with colorblind mode

--- app/todolist/screens/ToDoList.js
+++ app/todolist/screens/ToDoList.js
@@ -11,27 +11,76 @@ import NavBar from '../../shared/components/NavBar';
 
 const ToDoList = ({ navigation }) => {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles().container}>
       <View>
-        <Text>
+        <Text style={styles().textReg}>
           Use this to-do list to keep track of upcoming tasks. Let's do our best
           to have another productive day!
         </Text>
         <Image
           source={require('../../shared/assets/icon.png')}
-          style={styles.avatar}
+          style={styles().avatar}
         />
       </View>
       <View>
-        <Button title='<' />
-        <Button title='Categories' />
-        <Button title='>' />
+        <Button
+          title='<'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='Categories'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='>'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
       </View>
       <View>
-        <Button title='task1' />
-        <Button title='task2' />
-        <Button title='task3' />
-        <Button title='task4' />
+        <Button
+          title='task1'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='task2'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='task3'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='task4'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
       </View>
       <NavBar todo={true} navigation={navigation} />
     </SafeAreaView>
@@ -40,10 +89,12 @@ const ToDoList = ({ navigation }) => {
 
 export { ToDoList };
 
-const styles = StyleSheet.create({
+const styles = () => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F6EFED',
+    backgroundColor: global.colorblindMode
+      ? global.cb_pageBackgroundColor
+      : global.pageBackgroundColor,
   },
   avatar: {
     width: 75,
@@ -55,5 +106,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: 100,
     height: 50,
+  },
+  textReg: {
+    color: global.colorblindMode
+      ? global.cb_textColor
+      : global.textColor,
+    textDecorationLine: 'none',
+    textAlign: 'left',
   },
 });
