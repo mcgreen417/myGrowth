6e515f0f1eb54d4d5f3286e5560819f6d212987e
emsonn <eabrahamson@knights.ucf.edu>
2021-03-18 21:25:46 -0400

    Updated HistoryMedication with history modal component and colorblind mode

--- app/history/screens/HistoryMedication.js
+++ app/history/screens/HistoryMedication.js
@@ -11,306 +11,179 @@ import {
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import NavBar from '../../shared/components/NavBar';
+import HistorySelectACategory from '../../shared/components/HistorySelectACategory';
 
 function HistoryMedication({ navigation }) {
-    const [modalVisible, setModalVisible] = useState(false);
-  
-    const [selectmedication, setMedication] = useState('unselected');
-  
-    return (
-      <SafeAreaView style={styles.container}>
-        {/* Modal */}
-        <View>
-          <Modal
-            animationType="none"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-              setModalVisible(!modalVisible);
-            }}
-          >
-            <View style={styles.centeredView}>
-              {/* Dismiss Bar */}
-              <View style={styles.inlineRowBackgrd}>
-                  {/* add category chooser modal here */}
-                  { /* image asset */ }
-                  <View>
-                      <Image style={{width: 20, height: 20}} source={require('../../shared/assets/icon.png')}/>
-                  </View>
-                  <View>
-                      <Text style={{color: 'white'}}>Select a Category</Text>
-                  </View>
-                  <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
-                      { /* X asset */ }
-                      <View>
-                          <Image source={require('../../shared/assets/close.png')}/>
-                      </View>
-                  </TouchableOpacity>
-              </View>
-  
-              {/* Body of modal */}
-              <View style={{backgroundColor: '#F6EFED', width: 300, height: 350,}}>
-                {/* Buttons on modal */}
-                <View style={styles.inlineRowModal}>
-                    { /* health entries */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryHealthEntries');
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Health Entries</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* mood */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                            navigation.navigate('HistoryMood');
-                            setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Mood</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* stress */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryStress');
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Stress</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* daily activities */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryDailyActivities1');
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Daily Activities</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-  
-                <View style={styles.inlineRowModal}>
-                    { /* period tracking */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryPeriodTracking');
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Period Tracking</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* weight */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryWeight');
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Weight</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* gen health */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryGeneralHealth1')
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>General Health</Text>
-                        </TouchableOpacity>
-                    </View>
-  
-                    { /* medicine */ }
-                    <View>
-                        <TouchableOpacity onPress={() => {
-                          setModalVisible(!modalVisible);
-                        }}>
-                            <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                            <Text>Medicine</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-  
-                <View style={styles.inlineRowModal}>
-                  { /* sleep */ }
-                  <View>  
-                      <TouchableOpacity onPress={() => {
-                        navigation.navigate('HistorySleep1');
-                        setModalVisible(!modalVisible);
-                      }}>
-                          <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                          <Text>Sleep</Text>
-                      </TouchableOpacity>
-                  </View>
-  
-                  { /* meal tracking */ }
-                  <View>
-                      <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryMealTracking');
-                          setModalVisible(!modalVisible);
-                        }}>
-                          <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                          <Text>Meal Tracking</Text>
-                      </TouchableOpacity>
-                  </View>
-  
-                  { /* fitness */ }
-                  <View>
-                      <TouchableOpacity onPress={() => {
-                          navigation.navigate('HistoryFitness1');
-                          setModalVisible(!modalVisible);
-                        }}>
-                          <Image style={styles.chooserImg} source={require('../../shared/assets/icon.png')}/>
-                          <Text>Fitness</Text>
-                      </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </Modal>
-        </View>
-  
-        <View>
-          <Text>
-            View your medication history and our predictions for potential effects
-            stemming from your medication.
-          </Text>
-          <Image 
-            source={require('../../shared/assets/icon.png')} 
-            style={styles.avatar}
-          />
-        </View>
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectmedication, setMedication] = useState('unselected');
 
-        {/* page divider */}
-        <View style={styles.dividerView}>
-          <View style={styles.divider} />
-        </View>
-        <View>
-          <Button title='History' />
-          <Button title='Correlations' />
-          <TouchableOpacity style={styles.buttons} onPress={() => setModalVisible(true)}>
-            <View style={styles.inlineRow}>
-              <Text style={styles.textReg}>Categories</Text>
-              <View>
-              <Image source={require('../../shared/assets/transit_enterexit.png')} />
-              </View>
+  return (
+    <SafeAreaView style={styles().container}>
+      {/* Modal + each of the navigable history pages */}
+      <HistorySelectACategory
+        setModalView={setModalVisible}
+        showModalView={modalVisible}
+        navigation={navigation}
+      />
+      <View>
+        <Text style={styles().bodyText}>
+          View your medication history and our predictions for potential effects
+          stemming from your medication.
+        </Text>
+        <Image 
+          source={require('../../shared/assets/icon.png')} 
+          style={styles().avatar}
+        />
+      </View>
+
+      {/* page divider */}
+      <View style={styles().dividerView}>
+        <View style={styles().divider} />
+      </View>
+      <View>
+        <Button
+          title='History'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <Button
+          title='Correlations'
+          color={
+            global.colorblindMode
+              ? global.cb_optionButtonsColor
+              : global.optionButtonsColor
+          }
+        />
+        <TouchableOpacity style={styles().buttons} onPress={() => setModalVisible(true)}>
+          <View style={styles().inlineRow}>
+            <Text style={styles().textReg}>Categories</Text>
+            <View>
+            <Image source={require('../../shared/assets/transit_enterexit.png')} />
             </View>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text>SELECT MEDICATION</Text>
-          <Picker
-            selectedValue={selectmedication}
-            style={{ height: 50, width: 200 }}
-            onValueChange={(itemValue, itemIndex) => setMedication(itemValue)}
-          >
-            <Picker.Item label='Select one...' value='unselected' />
-            <Picker.Item label='(Medicine 1)' value='medicine_1' />
-            <Picker.Item label='(Medicine 2)' value='medicine_2' />
-            <Picker.Item label='(Medicine 3)' value='medicine_3' />
-            <Picker.Item label='(Medicine 4)' value='medicine_4' />
-            <Picker.Item label='(Medicine 5)' value='medicine_5' />
-            <Picker.Item label='(Medicine 6)' value='medicine_6' />
-          </Picker>
-        </View>
-        {/* page divider */}
-        <View style={styles.dividerView}>
-          <View style={styles.divider} />
-        </View>
-        <View>
-          <Text>
-            Based on our analysis, on days when you don't take your prescribed
-            medication, you often report experiencing the following...
-          </Text>
-          {/* <NotTakeMedicationAnalysis /> */}
-        </View>
-        <View>
-          <Text>
-            Based on our analysis, on days when you take your prescribed
-            medication late, you often report experiencing the following...
-          </Text>
-          {/* <TakeLateMedicationAnalysis /> */}
-        </View>
-        <NavBar history={true} navigation={navigation} />
-      </SafeAreaView>
-    );
-  };
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text style={styles().bodyText}>SELECT MEDICATION</Text>
+        <Picker
+          selectedValue={selectmedication}
+          style={{ height: 50, width: 200 }}
+          onValueChange={(itemValue, itemIndex) => setMedication(itemValue)}
+        >
+          <Picker.Item label='Select one...' value='unselected' />
+          <Picker.Item label='(Medicine 1)' value='medicine_1' />
+          <Picker.Item label='(Medicine 2)' value='medicine_2' />
+          <Picker.Item label='(Medicine 3)' value='medicine_3' />
+          <Picker.Item label='(Medicine 4)' value='medicine_4' />
+          <Picker.Item label='(Medicine 5)' value='medicine_5' />
+          <Picker.Item label='(Medicine 6)' value='medicine_6' />
+        </Picker>
+      </View>
+      {/* page divider */}
+      <View style={styles().dividerView}>
+        <View style={styles().divider} />
+      </View>
+      <View>
+        <Text style={styles().bodyText}>
+          Based on our analysis, on days when you didn't take your prescribed
+          medication, you often report experiencing the following...
+        </Text>
+        {/* <NotTakeMedicationAnalysis /> */}
+      </View>
+      <View>
+        <Text style={styles().bodyText}>
+          Based on our analysis, on days when you take your prescribed
+          medication late, you often report experiencing the following...
+        </Text>
+        {/* <TakeLateMedicationAnalysis /> */}
+      </View>
+      <NavBar history={true} navigation={navigation} />
+    </SafeAreaView>
+  );
+};
 
 export default HistoryMedication;
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F6EFED',
-    },
-    avatar: {
-      width: 75,
-      height: 75,
-      marginRight: 24,
-    },
-    buttons: {
-      marginTop: 10,
-      marginBottom: 10,
-      width: 80,
-      height: 25,
-      backgroundColor: '#4CB97A',
-    },
-    centeredView: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 8,
-    },
-    chooserImg: {
-      borderWidth: 1,
-      borderColor: '#4CB97A',
-      width: 40,
-      height: 40,
-    },
-    divider: {
-      flex: 1,
-      height: 1,
-      backgroundColor: '#816868',
-      marginLeft: 20,
-      marginRight: 20,
-    },
-    dividerView: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginTop: 20,
-      marginBottom: 20,
-    },
-    inlineRow: {
-      flexDirection: 'row',
-      width: '90%',
-      alignItems: 'center',
-    },
-    inlineRowBackgrd: {
-      backgroundColor: '#A5DFB2', 
-      width: 300, 
-      height: 30,
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    inlineRowModal: {
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-    textReg: {
-      color: 'white',
-      textDecorationLine: 'none',
-      textAlign: 'center',
-      fontSize: 12,
-    },
+const styles = () => StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: global.colorblindMode
+      ? global.cb_pageBackgroundColor
+      : global.pageBackgroundColor,
+  },
+  avatar: {
+    width: 75,
+    height: 75,
+    marginRight: 24,
+  },
+  buttons: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: 80,
+    height: 25,
+    backgroundColor: global.colorblindMode
+      ? global.cb_optionButtonsColor
+      : global.optionButtonsColor,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+  },
+  chooserImg: {
+    borderWidth: 1,
+    borderColor: global.colorblindMode
+      ? global.cb_optionButtonsColor
+      : global.optionButtonsColor,
+    width: 40,
+    height: 40,
+  },
+  divider: {
+    flex: 1,
+    height: 1,
+    backgroundColor: global.colorblindMode
+      ? global.cb_contentDividerColor
+      : global.contentDividerColor,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  dividerView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  inlineRow: {
+    flexDirection: 'row',
+    width: '90%',
+    alignItems: 'center',
+  },
+  inlineRowBackgrd: {
+    backgroundColor: global.colorblindMode
+      ? global.cb_optionButtonsColor
+      : global.optionButtonsColor, 
+    width: 300, 
+    height: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inlineRowModal: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textReg: {
+    color: 'white',
+    textDecorationLine: 'none',
+    textAlign: 'center',
+    fontSize: 12,
+  },
+  bodyText: {
+    color: global.colorblindMode
+      ? global.cb_textColor
+      : global.textColor,
+  },
 });
