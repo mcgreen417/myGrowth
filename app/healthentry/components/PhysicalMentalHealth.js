eba04cf19c37a800325007fb9a976030c586cac2
Enelson Castro <enecastro@knights.ucf.edu>
2021-03-23 17:06:08 -0400

    Added Physical Mental Health

--- app/healthentry/components/PhysicalMentalHealth.js
+++ app/healthentry/components/PhysicalMentalHealth.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Switch,
+  Button,
+  TextInput,
+} from 'react-native';
 
 const PhysicalMentalHealth = () => {
+  const [hadPeriod, setHadPeriod] = useState(false);
+
   return (
-    <View>
+    <View style={{ width: '80%' }}>
       <Text>Physical & Mental Health</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Text>Did you have your period today?</Text>
+        <Switch
+          trackColor={{ false: '#E5E5E5', true: '#9AD2AF' }}
+          thumbColor={hadPeriod ? '#4CB97A' : '#f4f3f4'}
+          ios_backgroundColor='#3e3e3e'
+          onValueChange={() => setHadPeriod(!hadPeriod)}
+          value={hadPeriod}
+        />
+      </View>
+      <Text>
+        If you have weighed yourself today, how much do you weigh? (Leave field
+        blank if you are unsure.)
+      </Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TextInput
+          placeholder='#'
+          style={{ borderBottomColor: '#C4BEBD', borderBottomWidth: 1 }}
+        />
+        <Text>lbs</Text>
+      </View>
+      <Text>
+        Have you experienced any unusual physical or mental health symptoms
+        today?
+      </Text>
+      <View style={{ width: '50%' }}>
+        <Button title='+ Add Symptoms' />
+      </View>
     </View>
   );
 };
