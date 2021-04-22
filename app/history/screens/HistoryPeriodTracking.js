4dfe4f7f04b5b4f33c73ac78d5f1c2f96e8cf320
adenrae <99aden@gmail.com>
2021-04-22 18:26:09 -0400

    fixed prediction

--- app/history/screens/HistoryPeriodTracking.js
+++ app/history/screens/HistoryPeriodTracking.js
@@ -16,6 +16,21 @@ import NavBar from '../../shared/components/NavBar';
 import TabBarAndContent from '../../shared/components/TabBarAndContent';
 import HistorySelectACategory from '../../shared/components/HistorySelectACategory';
 
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'June',
+  'July',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
 function HistoryPeriodTracking({ route, navigation }) {
   const data = route.params.data;
   const settings = route.params.settings;
@@ -25,6 +40,9 @@ function HistoryPeriodTracking({ route, navigation }) {
     const arr = initDisplayData(data);
     const labels = getTimestamps(data);
     const commits = genCommits(arr, labels);
+    const today = new Date();
+    var predictDate = new Date();
+    predictDate.setDate((today.getDate() + getPeriod(data)));
 
     return (
       <SafeAreaView style={styles().container}>
@@ -112,7 +130,7 @@ function HistoryPeriodTracking({ route, navigation }) {
                       : global.navBarCurrentIconColor,
                   }}>
                     <View style={{ marginVertical: 12 }}>
-                      <Text style={styles().textNumber}>02</Text>
+                      <Text style={styles().textNumber}>{getPeriod(data)}</Text>
                       <Text style={styles().textLight}>days until your{"\n"} next period</Text>
                     </View>
                 </View>
@@ -121,8 +139,8 @@ function HistoryPeriodTracking({ route, navigation }) {
               {/* Period statistics */}
               <View style={{ width: '45%' }}>
                 <View style={{ flexDirection: 'row', }}>
-                  <Text style={styles().text}>Next period predicted to occur in
-                    <Text style={styles().textAltGreen}> {getPeriod(data)} days!{"\n"}</Text>
+                  <Text style={styles().text}>Next period predicted to occur on
+                    <Text style={styles().textAltGreen}> {monthNames[predictDate.getMonth()]} {predictDate.getDate()}!{"\n"}</Text>
                   </Text>
                 </View>
                 {/*
@@ -322,10 +340,10 @@ function getPeriod(data) {
   var dateOfLastPeriod = date;
 
   //find date of last period
-  for(var i = data.periodData.length; i >= 0; i++) {
+  for(var i = data.periodData.length; i >= 0; i--) {
     if(data.periodData[i] === 1) {
-      dateOfLastPeriod = date.getDate() (data.periodData.length - i);
-      daysUntil = Math.abs(daysUntil - ((curDate.getDate() - dateOfLastPeriod.getDate()) % 28));
+      dateOfLastPeriod = new Date(date.getDate() - (data.periodData.length - i));
+      daysUntil = Math.abs(daysUntil - ((curDate.getDate() - dateOfLastPeriod.getDate()))) % 28;
 
       break;
     }
